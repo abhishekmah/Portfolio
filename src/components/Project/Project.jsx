ca09057f109476445d77f05f52f665743267e1d7
Abhishek Mahato <mahatoabhishek123@gmail.com>
2021-11-01 15:31:11 +0530

    resume file updated

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,158 +1,216 @@
-
-import { useState, useRef } from "react";
-import "./project.scss";
+import { useState, useRef } from 'react';
+import './project.scss';
 import Slider from 'infinite-react-carousel';
 import { v4 as uuidv4 } from 'uuid';
 
-
 export default function Project() {
+    const data = [
+        {
+            id: '1',
+            icon: 'hotelL.png',
+            linkSite: 'https://hotelsclone.netlify.app',
+            linkGithub: 'https://github.com/abhishekmah/Project-Hotels.com',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/hotels-com-a-cloned-version-57165d6eef3f',
+            title: 'Hotels.com',
+            desc: 'Hotels.com is a leading provider of hotel accommodation worldwide, offering booking services through localized websites.',
+            img: 'hotel.png',
+        },
+        {
+            id: '2',
+            icon: 'angelL.png',
+            linkSite: 'https://projectangel.netlify.app',
+            linkGithub: 'https://github.com/vshubhams/project-angel',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/a-cloned-version-of-angel-co-d7718733313c',
+            title: 'AngelList',
+            desc: 'AngelList is a U.S. website for startups, angel investors, and job-seekers looking to work at startups.',
+            img: 'angel.png',
+        },
+        {
+            id: '3',
+            icon: 'myglamm-logo.png',
+            linkSite: 'https://myglammclone.netlify.app',
+            linkGithub: 'https://github.com/tauseef-stack/myGlamm',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/myglamm-com-a-cloned-version-703f06e7e9cf',
+            title: 'My Glamm',
+            desc: 'MyGlamm is a direct-to-consumer beauty brand that offers an online beauty products marketplace.',
+            img: 'glamm.png',
+        },
+        {
+            id: '4',
+            icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXkhxN0PlWh2u8atnvS0IlkgehwEbUTXaDIA&usqp=CAU',
+            linkSite: 'https://tumblrclone.netlify.app/',
+            linkGithub: 'https://github.com/abhishekmah/Tumblr-Project',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/tumblr-com-a-cloned-version-49e9d2391dca',
+            title: 'Tumblr',
+            desc: 'Tumblr.com is an American microblogging and social networking website founded in 2007.',
+            img: 'tumblr.png',
+        },
+        {
+            id: '1',
+            icon: 'hotelL.png',
+            linkSite: 'https://hotelsclone.netlify.app',
+            linkGithub: 'https://github.com/abhishekmah/Project-Hotels.com',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/hotels-com-a-cloned-version-57165d6eef3f',
+            title: 'Hotels.com',
+            desc: 'Hotels.com is a leading provider of hotel accommodation worldwide, offering booking services through localized websites.',
+            img: 'hotel.png',
+        },
+        {
+            id: '2',
+            icon: 'angelL.png',
+            linkSite: 'https://projectangel.netlify.app',
+            linkGithub: 'https://github.com/vshubhams/project-angel',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/a-cloned-version-of-angel-co-d7718733313c',
+            title: 'AngelList',
+            desc: 'AngelList is a U.S. website for startups, angel investors, and job-seekers looking to work at startups.',
+            img: 'angel.png',
+        },
+        {
+            id: '3',
+            icon: 'myglamm-logo.png',
+            linkSite: 'https://myglammclone.netlify.app',
+            linkGithub: 'https://github.com/tauseef-stack/myGlamm',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/myglamm-com-a-cloned-version-703f06e7e9cf',
+            title: 'My Glamm',
+            desc: 'MyGlamm is a direct-to-consumer beauty brand that offers an online beauty products marketplace.',
+            img: 'glamm.png',
+        },
+        {
+            id: '4',
+            icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXkhxN0PlWh2u8atnvS0IlkgehwEbUTXaDIA&usqp=CAU',
+            linkSite: 'https://tumblrclone.netlify.app/',
+            linkGithub: 'https://github.com/abhishekmah/Tumblr-Project',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/tumblr-com-a-cloned-version-49e9d2391dca',
+            title: 'Tumblr',
+            desc: 'Tumblr.com is an American microblogging and social networking website founded in 2007.',
+            img: 'tumblr.png',
+        },
+        {
+            id: '1',
+            icon: 'hotelL.png',
+            linkSite: 'https://hotelsclone.netlify.app',
+            linkGithub: 'https://github.com/abhishekmah/Project-Hotels.com',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/hotels-com-a-cloned-version-57165d6eef3f',
+            title: 'Hotels.com',
+            desc: 'Hotels.com is a leading provider of hotel accommodation worldwide, offering booking services through localized websites.',
+            img: 'hotel.png',
+        },
+        {
+            id: '2',
+            icon: 'angelL.png',
+            linkSite: 'https://projectangel.netlify.app',
+            linkGithub: 'https://github.com/vshubhams/project-angel',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/a-cloned-version-of-angel-co-d7718733313c',
+            title: 'AngelList',
+            desc: 'AngelList is a U.S. website for startups, angel investors, and job-seekers looking to work at startups.',
+            img: 'angel.png',
+        },
+        {
+            id: '3',
+            icon: 'myglamm-logo.png',
+            linkSite: 'https://myglammclone.netlify.app',
+            linkGithub: 'https://github.com/tauseef-stack/myGlamm',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/myglamm-com-a-cloned-version-703f06e7e9cf',
+            title: 'My Glamm',
+            desc: 'MyGlamm is a direct-to-consumer beauty brand that offers an online beauty products marketplace.',
+            img: 'glamm.png',
+        },
+        {
+            id: '4',
+            icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXkhxN0PlWh2u8atnvS0IlkgehwEbUTXaDIA&usqp=CAU',
+            linkSite: 'https://tumblrclone.netlify.app/',
+            linkGithub: 'https://github.com/abhishekmah/Tumblr-Project',
+            linkBlog:
+                'https://medium.com/@mahatoabhishek123/tumblr-com-a-cloned-version-49e9d2391dca',
+            title: 'Tumblr',
+            desc: 'Tumblr.com is an American microblogging and social networking website founded in 2007.',
+            img: 'tumblr.png',
+        },
+    ];
 
-  const data = [
-    {
-      id: "1",
-      icon: "hotelL.png",
-      linkSite: "https://hotelsclone.netlify.app",
-      linkGithub: "https://github.com/abhishekmah/Project-Hotels.com",
-      linkBlog: "https://medium.com/@mahatoabhishek123/hotels-com-a-cloned-version-57165d6eef3f",
-      title: "Hotels.com",
-      desc:
-        "Hotels.com is a leading provider of hotel accommodation worldwide, offering booking services through localized websites.",
-      img:
-        "hotel.png",
-    },
-    {
-      id: "2",
-      icon: "angelL.png",
-      linkSite: "https://projectangel.netlify.app",
-      linkGithub: "https://github.com/vshubhams/project-angel",
-      linkBlog: "https://medium.com/@mahatoabhishek123/a-cloned-version-of-angel-co-d7718733313c",
-      title: "AngelList",
-      desc:
-        "AngelList is a U.S. website for startups, angel investors, and job-seekers looking to work at startups.",
-      img:
-        "angel.png",
-    },
-    {
-      id: "3",
-      icon: "myglamm-logo.png",
-      linkSite: "https://myglammclone.netlify.app",
-      linkGithub: "https://github.com/tauseef-stack/myGlamm",
-      linkBlog: "https://medium.com/@mahatoabhishek123/myglamm-com-a-cloned-version-703f06e7e9cf",
-      title: "My Glamm",
-      desc:
-        "MyGlamm is a direct-to-consumer beauty brand that offers an online beauty products marketplace.",
-      img:
-        "glamm.png",
-    },
-    {
-      id: "1",
-      icon: "hotelL.png",
-      linkSite: "https://hotelsclone.netlify.app",
-      linkGithub: "https://github.com/abhishekmah/Project-Hotels.com",
-      linkBlog: "https://medium.com/@mahatoabhishek123/hotels-com-a-cloned-version-57165d6eef3f",
-      title: "Hotels.com",
-      desc:
-        "Hotels.com is a leading provider of hotel accommodation worldwide, offering booking services through localized websites.",
-      img:
-        "hotel.png",
-    },
-    {
-      id: "2",
-      icon: "angelL.png",
-      linkSite: "https://projectangel.netlify.app",
-      linkGithub: "https://github.com/vshubhams/project-angel",
-      linkBlog: "https://medium.com/@mahatoabhishek123/a-cloned-version-of-angel-co-d7718733313c",
-      title: "AngelList",
-      desc:
-        "AngelList is a U.S. website for startups, angel investors, and job-seekers looking to work at startups.",
-      img:
-        "angel.png",
-    },
-    {
-      id: "3",
-      icon: "myglamm-logo.png",
-      linkSite: "https://myglammclone.netlify.app",
-      linkGithub: "https://github.com/tauseef-stack/myGlamm",
-      linkBlog: "https://medium.com/@mahatoabhishek123/myglamm-com-a-cloned-version-703f06e7e9cf",
-      title: "My Glamm",
-      desc:
-        "MyGlamm is a direct-to-consumer beauty brand that offers an online beauty products marketplace.",
-      img:
-        "glamm.png",
-    },
-    {
-      id: "1",
-      icon: "hotelL.png",
-      linkSite: "https://hotelsclone.netlify.app",
-      linkGithub: "https://github.com/abhishekmah/Project-Hotels.com",
-      linkBlog: "https://medium.com/@mahatoabhishek123/hotels-com-a-cloned-version-57165d6eef3f",
-      title: "Hotels.com",
-      desc:
-        "Hotels.com is a leading provider of hotel accommodation worldwide, offering booking services through localized websites.",
-      img:
-        "hotel.png",
-    },
-    {
-      id: "2",
-      icon: "angelL.png",
-      linkSite: "https://projectangel.netlify.app",
-      linkGithub: "https://github.com/vshubhams/project-angel",
-      linkBlog: "https://medium.com/@mahatoabhishek123/a-cloned-version-of-angel-co-d7718733313c",
-      title: "AngelList",
-      desc:
-        "AngelList is a U.S. website for startups, angel investors, and job-seekers looking to work at startups.",
-      img:
-        "angel.png",
-    },
-    {
-      id: "3",
-      icon: "myglamm-logo.png",
-      linkSite: "https://myglammclone.netlify.app",
-      linkGithub: "https://github.com/tauseef-stack/myGlamm",
-      linkBlog: "https://medium.com/@mahatoabhishek123/myglamm-com-a-cloned-version-703f06e7e9cf",
-      title: "My Glamm",
-      desc:
-        "MyGlamm is a direct-to-consumer beauty brand that offers an online beauty products marketplace.",
-      img:
-        "glamm.png",
-    },
+    return (
+        <div className="Project" id="Project">
+            <br />
 
-    
-  ];
-
-  return (
-    <div className="Project" id="Project">
-      <br />
-      
-      <h1>My Projects</h1>
-      <hr style={{width: 180, marginTop: "-18px",height: 4, backgroundColor: "#18d26e", border: "none"}}/>
-      <br />
-      <Slider autoplay={true} slidesToShow={2} centerMode={true} autoplayScroll={1} centerPadding={-60} arrows={true}>
-        {data.map((d) => (
-          <div key={uuidv4()} className="container">
-            <div className="item">
-              <div className="left">
-                <div className="leftContainer">
-                  <div className="imgContainer">
-                    <img src={d.icon} alt="" />
-                  </div>
-                  <h2 style={{color: "crimson"}}>{d.title}</h2>
-                  <p>{d.desc}</p>
-                  <div className="button">
-                  <a href= {d.linkBlog} target="blank" style=
-                  {{textDecoration: "none", color: "white"}}><button>Blog</button></a>
-                  <a href= {d.linkGithub} target="blank" style=
-                  {{textDecoration: "none", color: "white"}}><button style={{marginLeft: 10}}>Github</button></a>
-                  </div>
-                </div>
-              </div>
-              <div className="right">
-                <a href={d.linkSite} target="blank"><div><img src={d.img} alt=""/></div></a>
-              </div>
-            </div>
-          </div>
-           ))}
-        </Slider>
-    </div>   
-  );
+            <h1>My Projects</h1>
+            <hr
+                style={{
+                    width: 180,
+                    marginTop: '-18px',
+                    height: 4,
+                    backgroundColor: '#18d26e',
+                    border: 'none',
+                }}
+            />
+            <br />
+            <Slider
+                autoplay={true}
+                slidesToShow={2}
+                centerMode={true}
+                autoplayScroll={1}
+                centerPadding={-60}
+                arrows={true}
+            >
+                {data.map((d) => (
+                    <div key={uuidv4()} className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="leftContainer">
+                                    <div className="imgContainer">
+                                        <img src={d.icon} alt="" />
+                                    </div>
+                                    <h2 style={{ color: 'crimson' }}>
+                                        {d.title}
+                                    </h2>
+                                    <p>{d.desc}</p>
+                                    <div className="button">
+                                        <a
+                                            href={d.linkBlog}
+                                            target="blank"
+                                            style={{
+                                                textDecoration: 'none',
+                                                color: 'white',
+                                            }}
+                                        >
+                                            <button>Blog</button>
+                                        </a>
+                                        <a
+                                            href={d.linkGithub}
+                                            target="blank"
+                                            style={{
+                                                textDecoration: 'none',
+                                                color: 'white',
+                                            }}
+                                        >
+                                            <button style={{ marginLeft: 10 }}>
+                                                Github
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="right">
+                                <a href={d.linkSite} target="blank">
+                                    <div>
+                                        <img src={d.img} alt="" />
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,37 +1,60 @@
-import "./topbar.scss";
-import { Person, Mail } from "@material-ui/icons";
-import { FaDownload } from "react-icons/fa"
+import './topbar.scss';
+import { Person, Mail } from '@material-ui/icons';
+import { FaDownload } from 'react-icons/fa';
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
-  return (
-    <div className={"topbar " + (menuOpen && "active")}>
-      <div className="wrapper">
-        <div className="left">
-          <div className="logoImg"><img src="abhiLogo.png" /></div>
-          <div className="itemContainer">
-            <Person className="icon" />
-            <span>+91 7098822766</span>
-          </div>
-          <div className="itemContainer">
-            <Mail className="icon" />
-            <span>mahatoabhishek123@gmail.com</span>
-          </div>
+    return (
+        <div className={'topbar ' + (menuOpen && 'active')}>
+            <div className="wrapper">
+                <div className="left">
+                    <div className="logoImg">
+                        <img src="abhiLogo.png" />
+                    </div>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>+91 7098822766</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>mahatoabhishek123@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="itemContainer1">
+                        <a href="#intro">HOME</a>
+                    </div>
+                    <div className="itemContainer1">
+                        <a href="#about">ABOUT</a>
+                    </div>
+                    <div className="itemContainer1">
+                        <a href="#Project">PROJECT</a>
+                    </div>
+                    <div className="itemContainer1">
+                        <a href="#skill">SKILL</a>
+                    </div>
+                    <div className="itemContainer1">
+                        <a href="#contact">CONTACT</a>
+                    </div>
+                    <div className="itemContainer1" id="lastDiv">
+                        <a
+                            href="https://drive.google.com/file/d/1gW1X5KnVRla6NMQZ3Adk42qvqBrK1Fjr/view?usp=sharing"
+                            target="blank"
+                            id="last"
+                        >
+                            RESUME <FaDownload className="icon1" />
+                        </a>
+                    </div>
+
+                    <div
+                        className="hamburger"
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    >
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="right">
-          <div className="itemContainer1"><a href="#intro">HOME</a></div>
-          <div className="itemContainer1"><a href="#about">ABOUT</a></div>
-          <div className="itemContainer1"><a href="#Project">PROJECT</a></div>
-          <div className="itemContainer1"><a href="#skill">SKILL</a></div>
-          <div className="itemContainer1"><a href="#contact">CONTACT</a></div>
-          <div className="itemContainer1" id="lastDiv"><a href="https://drive.google.com/file/d/1XMnQ9p6Jgj3BIITQDGvPHHIK9TmhgE5L/view?usp=sharing" target="blank" id="last">RESUME <FaDownload className="icon1"/></a></div>
-          
-          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
-            <span className="line1"></span>
-            <span className="line2"></span>
-            <span className="line3"></span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
